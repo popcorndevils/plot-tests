e1f2ca90620ad8bb628d3d5a09462125b60aae8d
Michael Coates <popcorndevils@gmail.com>
2022-02-20 13:38:43 -0500

    good overview of using plotly

--- test_plot/test_plot/_plotter.py
+++ test_plot/test_plot/_plotter.py
@@ -11,13 +11,13 @@ class Plotter:
         self._plot_display = w.Output()
         self._btn_show = w.Button(description = "Show")
         self._btn_show.on_click(self.handle_click)
-        
+
     def run(self):
         with self._output:
             clear_output()
             display(self.widgets)
         return self._output
-            
+
     @property
     def widgets(self):
         _widgets = w.VBox([
@@ -26,19 +26,36 @@ class Plotter:
             self._plot_display,
         ])
         return _widgets
-    
+
     def handle_click(self, _):
-        # https://plotly.com/python-api-reference/generated/plotly.graph_objects.html#module-plotly.graph_objects
+        # https://plotly.com/python-api-reference/generated/plotly.graph_objects.html#plotly.graph_objects.FigureWidget
         data = pd.DataFrame([{"day": f"day {x}", "hits": random.randint(0, 100)} for x in range(1, 100)])
         data2 = pd.DataFrame([{"day": f"day {x}", "hits": random.randint(0, 10)} for x in range(1, 100)])
-        
+
         g = go.FigureWidget()
-        
-        g.add_scatter(name = "full", x = data["day"], y = data["hits"], line = {"color": "blue", "width": 2})
-        g.add_scatter(name = "partial", x = data2["day"], y = data2["hits"], line = {"color": "red", "width": 2})
-        
+
+        t1 = go.Scatter(
+            name = "full",
+            x = data["day"],
+            y = data["hits"],
+            line = {"color": "blue", "width": 2},
+            hovertemplate = "Price: $%{y:.2f}" + "<br>Week: %{x}"
+        )
+
+        t2 = go.Scatter(
+            name = "partial",
+            x = data2["day"],
+            y = data2["hits"],
+            line = {"color": "red", "width": 2},
+            hovertemplate = "Price: $%{y}" + "<br>Week: %{x}"
+        )
+
+        g.add_trace(t1)
+        g.add_trace(t2)
+
+        # g.update_layout(hovermode = "x unified")
+        g.update_layout(hovermode = "x")
+
         with self._plot_display:
             clear_output()
             display(g)
-            
-    
